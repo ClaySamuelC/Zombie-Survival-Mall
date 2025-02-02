37daed64952d807349f1cf09387c05ded1a266fc
Jeremy <jeremy.mccray1337@gmail.com>
2025-02-01 22:46:16 -0500

    Each guy can gather now, gather indicators

--- Scripts/State_Machine/survivor_Gather_State.gd
+++ Scripts/State_Machine/survivor_Gather_State.gd
@@ -14,10 +14,14 @@ func Exit():
 func Update(_delta: float):
 	#Will get put in this state when they click G, select a unit, and then right click
 	if script_user.in_gather_zone:
-		script_user.current_zone.gather()
-		pass
+		script_user.gather_tick += 1
+		script_user.gather_indicator.visible = true
+		if script_user.gather_tick > script_user.gather_tick_timer:
+			script_user.current_zone.gather()
+			script_user.gather_tick = 0
+	
 	if script_user.gather_mode == false:
+		script_user.gather_indicator.visible = false
 		transitioned.emit(self,"Survivor_Attack_state")
-
 func Physics_Update(_delta: float):
 	pass
